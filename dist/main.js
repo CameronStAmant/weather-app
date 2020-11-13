0c972165f6331840abab7fb19c029e1cfb7c2c34
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-13 14:45:56 -0700

    add weather icon for easy understanding

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,9 @@
 const submitSearch = document.getElementById('submit');
-const displayResults = document.getElementById('results');
 const displayTemp = document.getElementById('temperature');
+const displayIcon = document.getElementById('icon');
 const fahrenheit = document.getElementById('fahrenheit');
 const celsius = document.getElementById('celsius');
+const background = document.body.style.background;
 let fTemp;
 let cTemp;
 let tempOption;
@@ -21,12 +22,15 @@ const search = async (city) => {
       { mode: 'cors' }
     );
     const results = await query.json();
+    const resultsIcon = results.weather[0].icon;
     if (tempOption === 'imperial') {
       fTemp = results.main.temp.toFixed(1);
-      displayTemp.innerHTML = `Temperature: ${fTemp}`;
+      displayTemp.innerHTML = `Temperature: ${fTemp} F`;
+      displayIcon.src = `http://openweathermap.org/img/wn/${resultsIcon}@2x.png`;
     } else if (tempOption === 'metric') {
       cTemp = results.main.temp.toFixed(1);
-      displayTemp.innerHTML = `Temperature: ${cTemp}`;
+      displayTemp.innerHTML = `Temperature: ${cTemp} C`;
+      displayIcon.src = `http://openweathermap.org/img/wn/${resultsIcon}@2x.png`;
     }
   } catch {
     displayTemp.innerHTML = '';
@@ -42,10 +46,10 @@ math.addEventListener('change', () => {
   }
   if (fahrenheit.checked === true) {
     fTemp = ((cTemp * 9) / 5 + 32).toFixed(1);
-    displayTemp.innerHTML = `Temperature: ${fTemp}`;
+    displayTemp.innerHTML = `Temperature: ${fTemp} F`;
   } else if (celsius.checked === true) {
     cTemp = (((fTemp - 32) * 5) / 9).toFixed(1);
-    displayTemp.innerHTML = `Temperature: ${cTemp}`;
+    displayTemp.innerHTML = `Temperature: ${cTemp} C`;
   }
 });
 
@@ -53,3 +57,5 @@ submitSearch.addEventListener('click', (e) => {
   e.preventDefault();
   search();
 });
+
+const changeBackground = () => {};
