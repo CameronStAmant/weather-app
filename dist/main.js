df9f8d8055fdd5c58132633fe990800676097ce5
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-13 14:17:27 -0700

    add temp conversion allow temp to be chosen before search add change temp after search

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,33 @@
 const submitSearch = document.getElementById('submit');
 const displayResults = document.getElementById('results');
 const displayTemp = document.getElementById('temperature');
+const fahrenheit = document.getElementById('fahrenheit');
+const celsius = document.getElementById('celsius');
+let fTemp;
+let cTemp;
+let tempOption;
+fahrenheit.checked = true;
 
 const search = async (city) => {
   const searchTerm = document.getElementById('city').value;
-  console.log(searchTerm);
+  if (fahrenheit.checked === true) {
+    tempOption = 'imperial';
+  } else if (celsius.checked === true) {
+    tempOption = 'metric';
+  }
   try {
     const query = await fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=' +
-        searchTerm +
-        '&units=imperial&appid=8053e75bd7110bdaaee3622d6c524570',
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=${tempOption}&appid=8053e75bd7110bdaaee3622d6c524570`,
       { mode: 'cors' }
     );
     const results = await query.json();
-    displayTemp.innerHTML = `Temperature: ${results.main.temp}`;
-    console.log(results.main);
+    if (tempOption === 'imperial') {
+      fTemp = results.main.temp.toFixed(1);
+      displayTemp.innerHTML = `Temperature: ${fTemp}`;
+    } else if (tempOption === 'metric') {
+      cTemp = results.main.temp.toFixed(1);
+      displayTemp.innerHTML = `Temperature: ${cTemp}`;
+    }
   } catch {
     displayTemp.innerHTML = '';
     alert(
@@ -22,6 +35,19 @@ const search = async (city) => {
     );
   }
 };
+
+math.addEventListener('change', () => {
+  if (fTemp == undefined && cTemp == undefined) {
+    return;
+  }
+  if (fahrenheit.checked === true) {
+    fTemp = ((cTemp * 9) / 5 + 32).toFixed(1);
+    displayTemp.innerHTML = `Temperature: ${fTemp}`;
+  } else if (celsius.checked === true) {
+    cTemp = (((fTemp - 32) * 5) / 9).toFixed(1);
+    displayTemp.innerHTML = `Temperature: ${cTemp}`;
+  }
+});
 
 submitSearch.addEventListener('click', (e) => {
   e.preventDefault();
